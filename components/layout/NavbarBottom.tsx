9c3a352eb72b92eaf09558bdb1638f05e69c82c5
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2026-04-29 02:50:22 +0600

    style: mobile navbar spacing and alignment tweaks

--- components/layout/NavbarBottom.tsx
+++ components/layout/NavbarBottom.tsx
@@ -16,21 +16,21 @@ const NavbarBottom = () => {
     <section className="w-full bg-[#242323] py-0 flex items-center h-[50px] overflow-hidden">
       <div className="w-full px-6 md:px-12 flex items-center h-full relative">
         {/* Social Icons - Left Aligned */}
-        <div className="flex-none flex items-center gap-4 sm:gap-5 text-white z-20">
+        <div className="flex-none flex items-center gap-2.5 sm:gap-5 text-white z-20 -ml-2 sm:ml-0">
           <Link href="https://facebook.com" target="_blank" className="hover:opacity-80 transition-all">
-            <img src="/socialicon-navbr/facebook.svg" alt="Facebook" className="w-5 h-5 sm:w-6 sm:h-6 object-contain brightness-0 invert" />
+            <img src="/socialicon-navbr/facebook.svg" alt="Facebook" className="w-[18px] h-[18px] sm:w-6 sm:h-6 object-contain brightness-0 invert" />
           </Link>
 
           <Link href="https://instagram.com" target="_blank" className="hover:opacity-80 transition-all">
-            <img src="/socialicon-navbr/instagram.svg" alt="Instagram" className="w-5 h-5 sm:w-6 sm:h-6 object-contain brightness-0 invert" />
+            <img src="/socialicon-navbr/instagram.svg" alt="Instagram" className="w-[18px] h-[18px] sm:w-6 sm:h-6 object-contain brightness-0 invert" />
           </Link>
 
           <Link href="https://tiktok.com" target="_blank" className="hover:opacity-80 transition-all flex items-center">
-            <img src="/socialicon-navbr/tiktok.svg" alt="TikTok" className="w-5 h-5 sm:w-6 sm:h-6 object-contain brightness-0 invert" />
+            <img src="/socialicon-navbr/tiktok.svg" alt="TikTok" className="w-[18px] h-[18px] sm:w-6 sm:h-6 object-contain brightness-0 invert" />
           </Link>
 
           <Link href="https://youtube.com" target="_blank" className="hover:opacity-80 transition-all">
-            <img src="/socialicon-navbr/youtube.svg" alt="YouTube" className="w-5 h-5 sm:w-6 sm:h-6 object-contain brightness-0 invert" />
+            <img src="/socialicon-navbr/youtube.svg" alt="YouTube" className="w-[18px] h-[18px] sm:w-6 sm:h-6 object-contain brightness-0 invert" />
           </Link>
         </div>
 
@@ -50,7 +50,7 @@ const NavbarBottom = () => {
           </div>
           {/* Gradients for smooth transition */}
           <div className="absolute inset-y-0 left-0 w-20 bg-gradient-to-r from-[#242323] to-transparent z-10" />
-          <div className="absolute inset-y-0 right-0 w-20 bg-gradient-to-l from-[#242323] to-transparent z-10" />
+          <div className="hidden sm:block absolute inset-y-0 right-0 w-20 bg-gradient-to-l from-[#242323] to-transparent z-10" />
         </div>
 
         {/* Order Now Button - Right Aligned */}
